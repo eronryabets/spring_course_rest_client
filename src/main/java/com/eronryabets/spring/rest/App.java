5eb377588edf9af6e44cf7e6a2db11a55eadc3aa
Ihor Ryabets <eronryabets@gmail.com>
2022-02-16 02:49:17 +0200

    MyConfig

--- src/main/java/com/eronryabets/spring/rest/App.java
+++ src/main/java/com/eronryabets/spring/rest/App.java
@@ -1,13 +1,21 @@
 package com.eronryabets.spring.rest;
 
+import com.eronryabets.spring.rest.configuration.MyConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(MyConfig.class);
+
+        Communication communication = context.getBean("communication", Communication.class);
+
+
     }
 }
