b5ad55f4e3e87b28604191d34dc3f3e2c89fc7c1
Ihor Ryabets <eronryabets@gmail.com>
2022-02-16 21:20:08 +0200

    Add methods : getEmployee; saveEmployee; deleteEmployee.

--- src/main/java/com/eronryabets/spring/rest/App.java
+++ src/main/java/com/eronryabets/spring/rest/App.java
@@ -1,7 +1,10 @@
 package com.eronryabets.spring.rest;
 
 import com.eronryabets.spring.rest.configuration.MyConfig;
+import com.eronryabets.spring.rest.entity.Employee;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
 
 /**
  * Hello world!
@@ -15,6 +18,19 @@ public class App
                 new AnnotationConfigApplicationContext(MyConfig.class);
 
         Communication communication = context.getBean("communication", Communication.class);
+//        //getAllEmployees
+//        List<Employee> allEmployees = communication.getAllEmployees();
+//        allEmployees.forEach(System.out::println);
+//        //getEmployee
+//        Employee empById = communication.getEmployee(6);
+//        System.out.println(empById);
+//        //add employee - saveEmployee - update
+//        Employee emp = new Employee("Sveta","Krihovnikova","IT",1200);
+//        emp.setId(19);
+//        communication.saveEmployee(emp);
+//        //delete
+//        communication.deleteEmployee(19);
+
 
 
     }
--- src/main/java/com/eronryabets/spring/rest/Communication.java
+++ src/main/java/com/eronryabets/spring/rest/Communication.java
@@ -28,13 +28,33 @@ public class Communication {
     }
 
     public Employee getEmployee(int id){
-        return null;
+
+        Employee employee = restTemplate.getForObject(URL + "/" + id
+                ,Employee.class);
+
+        return employee;
     }
 
-    private void saveEmployee(Employee employee){
+    public void saveEmployee(Employee employee){
+
+        int id = employee.getId();
+
+        if(id == 0){
+            ResponseEntity<String> responseEntity =
+                    restTemplate.postForEntity(URL, employee, String.class);
+            System.out.println("New employee was added to DB");
+            System.out.println(responseEntity.getBody());
+        }
+        else {
+            restTemplate.put(URL, employee);
+            System.out.println("Employee with ID " + id + " was updated");
+        }
 
     }
-    private void deleteEmployee(int id){
+    public void deleteEmployee(int id){
+
+        restTemplate.delete(URL + "/" + id);
+        System.out.println("Employee with ID " + id + " was deleted");
 
     }
 
